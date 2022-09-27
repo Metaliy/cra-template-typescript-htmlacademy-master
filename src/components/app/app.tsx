898e2b2e7589020049d09c02c1240c73d33bf181
Metaliy <darkspirtx@gmail.com>
2022-09-27 21:09:17 +0300

    1.0

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,31 @@
+import { Route } from 'react-router';
+import { Routes } from 'react-router-dom';
+import { BasketPage } from '../../pages/basket/basker';
+import { CatalogPage } from '../../pages/catalog/catalog';
+import { ProductPage } from '../../pages/product/product';
+import HistoryRouter from '../history-router/history-router';
+import browserHistory from './browser-history';
+
+
 function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+  return (
+    <HistoryRouter history={browserHistory}>
+      <Routes>
+        <Route
+          path={'/'}
+          element={<CatalogPage/>}
+        />
+        <Route
+          path={'/product'}
+          element={<ProductPage/>}
+        />
+        <Route
+          path={'/basket'}
+          element={<BasketPage/>}
+        />
+      </Routes>
+    </HistoryRouter>
+  );
 }
 
 export default App;
